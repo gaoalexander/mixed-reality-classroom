e78b959660a1e1e8b65284a28a6c5c8443542712
Alexandra Serralta <avs327@nyu.edu>
2019-06-05 22:41:12 -0400

    network update

--- networking/camera-server-unity/cloud-server-test.py
+++ networking/camera-server-unity/cloud-server-test.py
@@ -28,6 +28,7 @@ clientsocket2.setblocking(False)
 
 
 '''
+import socket
 import socketserver
 import threading
 import json
@@ -35,7 +36,7 @@ import json
 state = {}
 clients = {}
 
-class MyTCPHandler(socketserver.BaseRequestHandler):
+class ThreadedTCPHandler(socketserver.BaseRequestHandler):
     """
     The RequestHandler class for our server.
     It is instantiated once per connection to the server, and must
@@ -43,10 +44,10 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
     client.
     """
 
-    def handle(self):
-        clients[self.request.getpeername()[0] + ":" + str(self.request.getpeername()[1])] = self.request
-
+    '''
+    def recv_loop(self):
         while(True):
+            print(clients)
             try:
                 # self.request is the TCP socket connected to the client
                 self.data = self.request.recv(4096).strip()
@@ -72,11 +73,47 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
                     senddata["success"] = np.array_equal(combination_ids,target)
             for client in clients.values():
                     client.sendall(json.dumps(senddata).encode('utf-8'))
+                    '''
 
-HOST, PORT = "", 20390
+    def handle(self):
+        clients[self.request.getpeername()[0] + ":" + str(self.request.getpeername()[1])] = self.request
+        self.request.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        while(True):
+            print(clients)
+            try:
+                # self.request is the TCP socket connected to the client
+                self.data = self.request.recv(4096).strip()
+            except:
+                print ("cannot receive data")
+                return
+            if not self.data:
+                return
+            #print "{} wrote:".format(self.client_address[0])
+            print (self.data)
+            senddata = {}
+            array = self.data.decode('utf-8').split('`')
+            data = json.loads(array[-2])
+            if (data["type"] == "object"):
+                    if (data['uid'] in state and state[data['uid']]['lockid'] != data['lockid'] and state[data['uid']]['lockid'] != ""):
+                            print("object in use")
+                    else:
+                            state[data['uid']] = data
+                    senddata = state
+                    senddata["type"] = "object"
+            elif (data["type"] == "check"):
+                    senddata["type"] = "check"
+                    senddata["success"] = np.array_equal(combination_ids,target)
+            for client in clients.values():
+                    client.sendall(json.dumps(senddata).encode('utf-8'))
+                    
+
+class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
+    pass
+
+HOST, PORT = "", 20391
 
 # Create the server, binding to localhost on port 9999
-server = socketserver.TCPServer((HOST, PORT), MyTCPHandler)
+server = ThreadedTCPServer((HOST, PORT), ThreadedTCPHandler)
 
 # Activate the server; this will keep running until you
 # interrupt the program with Ctrl-C
@@ -84,12 +121,12 @@ t = threading.Thread(target=server.serve_forever)
 t.setDaemon(True) # don't hang on exit
 t.start()
 
-while(True):
-    pass
+#while(True):
+#    pass
 
 #server.serve_forever()
 #-----------------------------------------------------------------------------------
-'''
+
 TCP_IP_ADDRESS = "127.0.0.1"                                # LAN IP ADDRESS OF SERVER
 TCP_PORT_NO = 20380
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -146,7 +183,7 @@ while True:
                 print(detected)
                 cv2.imshow('local', imarr)
                 cv2.waitKey(1)
-                '''
+                
 '''
 
                 b_array = bytearray()
