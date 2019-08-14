e258cd26651ff17048ea21b1893bc1ff1b37aaf2
Aniol <asm745@nyu.edu>
2019-08-13 22:43:42 -0400

    Added glow effect script for Mira cameras, started looking into outline effects and shaders for hover over parts of the cell

---
+++ unity/MR_Classroom/Assets/Scripts/CamerasGlow.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MK.Glow.Legacy
+{
+    public class CamerasGlow : MonoBehaviour
+    {
+        [SerializeField] private float _intensity = .18f;
+
+        private void Start()
+        {
+            StartCoroutine(WaitAndAddGlow());
+        }
+
+        IEnumerator WaitAndAddGlow()
+        {
+            yield return new WaitForEndOfFrame();
+            foreach (Camera camera in GetComponentsInChildren<Camera>())
+            {
+                MKGlow mkGlow = camera.gameObject.AddComponent<MKGlow>();
+                mkGlow.workflow = Workflow.Selective;
+                mkGlow.bloomIntensity = _intensity;
+            }
+        }
+    }
+}
